963ceb752ee42e42eca3f8109db64f40034ae348
ejuarr00 <ejuarr00@estudiantes.unileon.es>
2019-11-07 11:18:12 +0100

    test

---
+++ src/es/unileon/prg1/date/DateException.java
@@ -0,0 +1,14 @@
+package es.unileon.prg1.date;
+
+public class DateException extends Exception {
+			
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6915090179170412071L;
+
+	public DateException(String msg){
+		super(msg);
+	}
+
+}
